82091eec21509f8171f01b811b65e04c777b111d
p00rna11 <s545523@nwmissouri.edu>
2021-11-19 02:12:25 -0600

    updated

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var passport = require('passport'); 
+var LocalStrategy = require('passport-local').Strategy; 
 var mongoose = require('mongoose');
 var pens = require("./models/pens");
 
@@ -66,6 +68,29 @@ var resourceRouter = require('./routes/resource');
 
 var app = express();
 
+
+//passport use for local strategy
+passport.use(new LocalStrategy( 
+  function(username, password, done) { 
+    Account.findOne({ username: username }, function (err, user) { 
+      if (err) { return done(err); } 
+      if (!user) { 
+        return done(null, false, { message: 'Incorrect username.' }); 
+      } 
+      if (!user.validPassword(password)) { 
+        return done(null, false, { message: 'Incorrect password.' }); 
+      } 
+      return done(null, user); 
+    }); 
+  } 
+
+  app.use(require('express-session')({ 
+    secret: 'keyboard cat', 
+    resave: false, 
+    saveUninitialized: false 
+  }));
+  app.use(passport.initialize()); 
+  app.use(passport.session()); 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -84,6 +109,14 @@ app.use('/pens', pensRouter);
 app.use('/addmods', addmodsRouter);
 app.use('/selector', selectorRouter);
 app.use('/', resourceRouter);
+// passport config 
+// Use the existing connection 
+// The Account model  
+var Account =require('./models/account')); 
+ 
+passport.use(new LocalStrategy(Account.authenticate())); 
+passport.serializeUser(Account.serializeUser()); 
+passport.deserializeUser(Account.deserializeUser()); 
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {

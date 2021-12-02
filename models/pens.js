e94080a29254ef8b09664ad77cf7c01ceef70178
p00rna11 <s545523@nwmissouri.edu>
2021-12-01 23:20:44 -0600

    updated

--- models/pens.js
+++ models/pens.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose")
 const pensSchema = mongoose.Schema({
     pens_name: String,
-    penstype: {String, required:true},
-    cost:{type: Number,required:true,
+    penstype: {String},
+    cost:{type: Number,
         min:[10,'low price'],
         max:[150,'costly pen']}
 })
